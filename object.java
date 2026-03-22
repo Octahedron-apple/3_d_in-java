e70cc1b45cbe8591ad8f36746d741216e19677cb
Octahedron-apple <bhavya1singh1@gmail.com>
2026-03-23 00:28:16 +0530

    added a constructor for point class

--- object.java
+++ object.java
@@ -6,4 +6,12 @@ public class object
 class point
   {
     public float[] position = new float[3]; 
+    point(float[] pos){
+      if (pos.length!=3){
+            throw new IllegalArgumentException("Invalid parameters");
+        }
+        else{
+            this.position=pos;
+        }
+    }
   }
