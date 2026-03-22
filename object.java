8f42432ba72274fce0cc366eea1a7eb7ca632ea6
Octahedron-apple <bhavya1singh1@gmail.com>
2026-03-23 00:31:42 +0530

    added an array in point class to store the 2d coordinates of the point after being rendered

--- object.java
+++ object.java
@@ -6,6 +6,7 @@ public class object
 class point
   {
     public float[] position = new float[3]; 
+    public float[] _2D_position = new float[2];
     point(float[] pos){
       if (pos.length!=3){
             throw new IllegalArgumentException("Invalid parameters");
