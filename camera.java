9e3012bd32fdf26e581b138e650716daca90bd26
Octahedron-apple <bhavya1singh1@gmail.com>
2026-03-23 00:23:10 +0530

    making a constructor for camera

--- camera.java
+++ camera.java
@@ -3,5 +3,15 @@ public class camera{
     // for x,y,z pos
     public float[] rotation = new float[3];
     // for x,y,z rotation
+    public float focal_length;
+    // focal length of the camera
+    camera(float[] pos,float[] rot, float focal){
+        if (focal<=0 && pos.length!=3 && rot.length!=3){
+            throw new IllegalArgumentException("Invalid parameters");
+        }
+        else{
+            
+        }
 
+    }
 }
