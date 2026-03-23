ac6a096d64b29d5e06b564ebf913e820efecd85d
Octahedron-apple <bhavya1singh1@gmail.com>
2026-03-23 21:30:33 +0530

    modified the render method to make it factor in rotation aswell

--- object.java
+++ object.java
@@ -79,9 +79,48 @@ class point {
 
   void render() {
     if (this.cam != null) {
-      float dx = this.position[0] - this.cam.position[0];
-      float dy = this.position[1] - this.cam.position[1];
-      float dz = this.position[2] - this.cam.position[2];
+      float[] d_pos = new float[3];
+      d_pos[0] = this.position[0] - this.cam.position[0];
+      d_pos[1] = this.position[1] - this.cam.position[1];
+      d_pos[2] = this.position[2] - this.cam.position[2];
+
+      for (int ax = 0; ax < 3; ax++) {
+        float a = this.cam.rotation[ax];
+        // any one of three axis 0,1,2
+        float[][] m = new float[3][3];
+        float rad = (float) Math.toRadians(a);
+        float c = (float) Math.cos(rad);
+        float s = (float) Math.sin(rad);
+
+        if (ax == 0) {
+          m = new float[][] {
+              { 1, 0, 0 },
+              { 0, c, -s },
+              { 0, s, c }
+          };
+        } else if (ax == 1) {
+          m = new float[][] {
+              { c, 0, s },
+              { 0, 1, 0 },
+              { -s, 0, c }
+          };
+        } else if (ax == 2) {
+          m = new float[][] {
+              { c, -s, 0 },
+              { s, c, 0 },
+              { 0, 0, 1 }
+          };
+        }
+
+        float[] np = new float[3];
+        for (int i = 0; i < 3; i++)
+          np[i] = m[i][0] * d_pos[0] + m[i][1] * d_pos[1] + m[i][2] * d_pos[2];
+        d_pos = np;
+      }
+
+      float dx = d_pos[0];
+      float dy = d_pos[1];
+      float dz = d_pos[2];
 
       if (dz != 0) {
         this._2D_position[0] = (dx * this.cam.focal_length) / dz;
